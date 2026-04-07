aeaaf6fdcdd7051fb66178dcd974cdda8693a3b6
canbeyler <ovecler2025@gmail.com>
2026-04-07 03:44:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
-const CACHE = 'ustayapi-v3';
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'manifest.json', 'data.json'])));
+const CACHE = 'ustayapi-v2';
+const STATIC = ['.','index.html','manifest.json','icon.svg','data.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/recharts/umd/Recharts.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})));
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.url.includes('api.github.com')||e.request.url.includes('raw.githubusercontent.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(r=>{
+      if(r) return r;
+      return fetch(e.request).then(res=>{
+        if(res&&res.ok&&(res.type==='basic'||res.type==='cors')){
+          const cl=res.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,cl));
+        }
+        return res;
+      }).catch(()=>caches.match('index.html'));
+    })
+  );
 });
