356a68241e7b5d1d17a5d935b4276ca361b08674
canbeyler <ovecler2025@gmail.com>
2026-04-07 03:26:20 +0300

    Refactor service worker caching strategy and assets

--- sw.js
+++ sw.js
@@ -1,30 +1,10 @@
-const CACHE = 'ustayapi-v2';
-const STATIC = ['.','index.html','manifest.json','icon.svg','data.json',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/recharts/umd/Recharts.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
-];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC).catch(()=>{})));
-  self.skipWaiting();
+const CACHE_NAME = 'ustayapi-v1';
+const ASSETS = ['index.html', 'manifest.json', 'data.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('api.github.com')||e.request.url.includes('raw.githubusercontent.com')) return;
-  e.respondWith(
-    caches.match(e.request).then(r=>{
-      if(r) return r;
-      return fetch(e.request).then(res=>{
-        if(res&&res.ok&&(res.type==='basic'||res.type==='cors')){
-          const cl=res.clone();
-          caches.open(CACHE).then(c=>c.put(e.request,cl));
-        }
-        return res;
-      }).catch(()=>caches.match('index.html'));
-    })
-  );
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
