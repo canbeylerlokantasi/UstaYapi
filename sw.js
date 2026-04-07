6e87fa159e338ab4569573d0cbbc1b09541f32c5
canbeyler <ovecler2025@gmail.com>
2026-04-07 03:39:44 +0300

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'ustayapi-v1';
-const ASSETS = ['index.html', 'manifest.json', 'data.json'];
-
+const CACHE = 'ustayapi-v3';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'manifest.json', 'data.json'])));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
